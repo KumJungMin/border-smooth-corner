4e60c22892ac61b8037eb1e2aeeb0b0b00b8456e
KumJungMin <rmawjdals@naver.com>
2024-06-04 17:29:14 +0900

    type: update type

--- index.ts
+++ index.ts
@@ -1,12 +1,3 @@
-type InputProperty = "--smooth-corners" | "--bg-color" | "--border-color" | "--border-width";
-
-const defaultConfig: Record<InputProperty, number | string> = {
-  "--smooth-corners": 4,
-  "--bg-color": "transparent",
-  "--border-color": "black",
-  "--border-width": 1,
-};
-
 class SmoothCornersPainter {
   static get inputProperties(): InputProperty[] {
     return [ "--smooth-corners", "--bg-color", "--border-color", "--border-width" ];
@@ -19,6 +10,12 @@ class SmoothCornersPainter {
    * @returns {number | string}
    * */ 
   propertyValue(properties, key: InputProperty): number | string {
+    const defaultConfig: Record<InputProperty, number | string> = {
+      "--smooth-corners": 4,
+      "--bg-color": "transparent",
+      "--border-color": "black",
+      "--border-width": 1,
+    };
     return properties.get(key)[0] || defaultConfig[key];
   }
 
@@ -26,16 +23,16 @@ class SmoothCornersPainter {
    * getSuperellipse - Generate points for a superellipse
    * @param {number} a - width
    * @param {number} b - height
-   * @param {number} n - smoothness
+   * @param {number} n - smoothness(draw a Squircle, n=4)
    * @param {number} strokeWidth - border width
    * @returns {Array<{x: number, y: number}>}
    * */ 
-  getSuperellipse(a, b, n, strokeWidth) {
-    if (Number.isNaN(n)) n = 4;
-    else if (n > 100) n = 100;
-    else if (n < 0.00000000001) n = 0.00000000001;
+  getSuperellipse(a, b, n = 4, strokeWidth) {
+    let m = n;
+    if (m > 100) m = 100;
+    else if (m < 0.00000000001) m = 0.00000000001;
 
-    const n2 = 2 / n;
+    const m2 = 2 / m;
     const steps = 360;
     const step = (2 * Math.PI) / steps;
 
@@ -43,8 +40,8 @@ class SmoothCornersPainter {
       const cosT = Math.cos(t);
       const sinT = Math.sin(t);
 
-      const x = Math.abs(cosT) ** n2 * a * Math.sign(cosT);
-      const y = Math.abs(sinT) ** n2 * b * Math.sign(sinT);
+      const x = Math.abs(cosT) ** m2 * a * Math.sign(cosT);
+      const y = Math.abs(sinT) ** m2 * b * Math.sign(sinT);
 
       // Adjust points to avoid border clipping
       const adjustX = x > 0 ? x - strokeWidth * 2 : x + strokeWidth * 2;
@@ -62,8 +59,8 @@ class SmoothCornersPainter {
    * @param {Map<string, Array<string>>} properties - CSS properties
    * */
   paint(ctx, geom, properties) {
-    const n = this.propertyValue(properties, "--smooth-corners");
-    const strokeWidth = this.propertyValue(properties, "--border-width");
+    const n = Number(this.propertyValue(properties, "--smooth-corners"));
+    const strokeWidth = Number(this.propertyValue(properties, "--border-width"));
     const bgColor = this.propertyValue(properties, "--bg-color");
     const borderColor = this.propertyValue(properties, "--border-color");
 
