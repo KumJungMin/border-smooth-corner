e6947d1dc3beaf33318b5314b1579e7c79865950
KumJungMin <rmawjdals@naver.com>
2024-06-04 16:05:02 +0900

    type: set type

--- index.ts
+++ index.ts
@@ -1,44 +1,52 @@
-// TODO: type checking for properties
-// TODO: allow defaultConfig
-// TODO: allow customConfig
+type InputProperty = "--smooth-corners" | "--bg-color" | "--border-color" | "--border-width";
+
+const defaultConfig: Record<InputProperty, number | string> = {
+  "--smooth-corners": 4,
+  "--bg-color": "transparent",
+  "--border-color": "black",
+  "--border-width": 1,
+};
 
 class SmoothCornersPainter {
-  static get inputProperties() {
-    return [
-      "--smooth-corners",
-      "--bg-color",
-      "--border-color",
-      "--border-width",
-    ];
+  static get inputProperties(): InputProperty[] {
+    return [ "--smooth-corners", "--bg-color", "--border-color", "--border-width" ];
   }
 
-  propertyValue(properties, key) {
-    const defaultConfig = {
-      "--smooth-corners": 4,
-      "--bg-color": "transparent",
-      "--border-color": "black",
-      "--border-width": 1,
-    };
-
+  /** 
+   * propertyValue - Get property value from CSS
+   * @param {Map<string, Array<string>>} properties - CSS properties
+   * @param {InputProperty} key - CSS property key
+   * @returns {number | string}
+   * */ 
+  propertyValue(properties, key: InputProperty): number | string {
     return properties.get(key)[0] || defaultConfig[key];
   }
 
+  /** 
+   * getSuperellipse - Generate points for a superellipse
+   * @param {number} a - width
+   * @param {number} b - height
+   * @param {number} n - smoothness
+   * @param {number} strokeWidth - border width
+   * @returns {Array<{x: number, y: number}>}
+   * */ 
   getSuperellipse(a, b, n, strokeWidth) {
     if (Number.isNaN(n)) n = 4;
-    if (n > 100) n = 100;
-    if (n < 0.00000000001) n = 0.00000000001;
+    else if (n > 100) n = 100;
+    else if (n < 0.00000000001) n = 0.00000000001;
 
     const n2 = 2 / n;
     const steps = 360;
     const step = (2 * Math.PI) / steps;
 
-    const points = (t) => {
+    const points = (t: number) => {
       const cosT = Math.cos(t);
       const sinT = Math.sin(t);
 
       const x = Math.abs(cosT) ** n2 * a * Math.sign(cosT);
       const y = Math.abs(sinT) ** n2 * b * Math.sign(sinT);
 
+      // Adjust points to avoid border clipping
       const adjustX = x > 0 ? x - strokeWidth * 2 : x + strokeWidth * 2;
       const adjustY = y > 0 ? y - strokeWidth * 2 : y + strokeWidth * 2;
 
@@ -47,6 +55,12 @@ class SmoothCornersPainter {
     return Array.from({ length: steps }, (_, i) => points(i * step));
   }
 
+  /** 
+   * paint - Paint the superellipse
+   * @param {CanvasRenderingContext2D} ctx - Canvas 2D context
+   * @param {DOMRectReadOnly} geom - Geometry of the element
+   * @param {Map<string, Array<string>>} properties - CSS properties
+   * */
   paint(ctx, geom, properties) {
     const n = this.propertyValue(properties, "--smooth-corners");
     const strokeWidth = this.propertyValue(properties, "--border-width");
@@ -58,17 +72,16 @@ class SmoothCornersPainter {
     const points = this.getSuperellipse(width, height, n, strokeWidth);
 
     ctx.setTransform(1, 0, 0, 1, width, height);
+
     ctx.beginPath();
     ctx.fillStyle = bgColor;
     ctx.strokeStyle = borderColor;
     ctx.lineWidth = strokeWidth; // Adjust line width for better anti-aliasing
 
-    for (let i = 0; i < points.length; i++) {
-      const { x, y } = points[i];
-      if (i === 0) ctx.moveTo(x, y);
+    for(let [i, { x, y }] of points.entries()) {
+      if (i == 0) ctx.moveTo(x, y);
       else ctx.lineTo(x, y);
     }
-
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
